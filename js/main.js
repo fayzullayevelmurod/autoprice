ee29d697e938dbe36f437bb865db40b000e928bd
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-11-17 21:50:16 +0500

    modal media the end

--- js/main.js
+++ js/main.js
@@ -3,11 +3,28 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".slider_btn_right",
       prevEl: ".slider_btn_left",
     },
-  });
+});
   
 var swiper = new Swiper(".mySwiperOne", {
     navigation: {
       nextEl: ".slider_btn_right_one",
       prevEl: ".slider_btn_left_one",
     },
-  });
+});
+
+
+const request_call = document.querySelector('.request_call');
+const modal_red_bg = document.querySelector('.modal_red_bg');
+const modal_red = document.querySelector('.modal_red');
+
+
+request_call.addEventListener('click', () => {
+  modal_red_bg.classList.toggle('active');
+  modal_red.classList.toggle('active');
+});
+
+
+modal_red_bg.addEventListener('click', () => {
+  modal_red_bg.classList.remove('active');
+  modal_red.classList.remove('active');
+});
